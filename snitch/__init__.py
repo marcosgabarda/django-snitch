557ae2a0e01184ffc552536507782fff39785457
Andrés Reverón Molina <andres@reveronmolina.me>
2021-05-12 13:06:32 +0200

    revert: update to django@3.2

--- snitch/__init__.py
+++ snitch/__init__.py
@@ -24,3 +24,6 @@ __version__ = "1.8.3"
 
 def autodiscover():
     autodiscover_modules("events", register_to=manager)
+
+
+default_app_config = "snitch.apps.SnitchConfig"
--- snitch/apps.py
+++ snitch/apps.py
@@ -2,11 +2,15 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class SnitchConfig(AppConfig):
-    """The default AppConfig for admin which does automatic discovery."""
+class SimpleSnitchConfig(AppConfig):
+    """Simple AppConfig which does not do automatic discovery."""
 
     name: str = "snitch"
     verbose_name: str = _("Snitch")
+
+
+class SnitchConfig(SimpleSnitchConfig):
+    """The default AppConfig for admin which does automatic discovery."""
 
     def ready(self):
         super().ready()
